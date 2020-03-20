dc9ea5f54b40e7ba3ca86d4badd7842f1fdeb2bd
R2d2cruz <51866042+R2d2cruz@users.noreply.github.com>
2020-03-20 17:30:09 -0500

    create new Estudent class

--- Estudiante.py
+++ Estudiante.py
@@ -1,94 +1,47 @@
 import pygame
+import os
 
+found = os.getcwd()
 
-class Estudiante:
-    name = ""
-    attack = 0
-    defense = 0
-    animations = []
-    x = 0
-    y = 0
-    realX = 320
-    realY = 180
-    actualFrame = pygame.image()
-    side = 0
-    velocityx = 0
-    velocityy = 0
-    actualAnimation = None
+class Estudiante(pygame.sprite.Sprite):
 
-    # 0 = front
-    # 1 = back
-    # 2 = right
-    # 3 = left
-
-    switcher = {
-        0: (0, -5),
-        1: (0, 5),
-        2: (5, 0),
-        3: (-5, 0)
-    }
-
-    commands = {
-    }
-
-    def __init__(self, name):
+    def __init__(self, position, name="Henry", *groups):
+        super().__init__(*groups)
         self.name = name
-        self.commands = {
-            pygame.K_UP: self.up(),
-            pygame.K_DOWN: self.DOWN(),
-            pygame.K_RIGHT: self.RIGHT(),
-            pygame.K_LEFT: self.LEFT()
-        }
+        self.sheet = pygame.image.load(found + "\\Assets\\" + self.name + ".png")
+        self.sheet.set_clip(pygame.Rect(64, 0, 34, 56))
+        self.image = self.sheet.surface(self.sheet.get_clip())
+        self.rect = self.sheet.get_Rect
+        self.rect.topleft = position
+        self.frame = 0
+        self.front = {0: (37, 1, 34, 56)}
+        self.back = {0: (1, 1, 34, 56)}
+        self.left = {0: (217, 1, 32, 56)}
+        self.right = {0: (251, 1, 32, 56)}
+        self.walk = {0: (357, 1, 34, 53), 1: (393, 1, 34, 53)}
+        self.backWalk = {0: (285, 1, 34, 54), 1: (321, 1, 34, 54)}
+        self.leftWalk = {0: (109, 1, 34, 56), 1: (181, 1, 34, 56)}
+        self.rightWalk = {0: (73, 1, 34, 56), 1: (145, 1, 34, 56)}
 
-    def setAttack(self, attack):
-        self.attack = attack
+    def get_frame(self, frame_set):
+        self.frame += 1
+        if self.frame > (len(frame_set) - 1):
+            self.frame = 0
+        return frame_set[self.frame]
 
-    def getAttack(self):
-        return self.attack
+    def clip(self, clipped_rect):
+        if type(clipped_rect) is dict:
+            self.sheet.set_clip(pygame.Rect(self.get_frame(clipped_rect)))
+        else:
+            self.sheet.set_clip(clipped_rect)
+        return clipped_rect
 
-    def getName(self):
-        return self.name
 
-    def act(self, delta, command):
-        self.states(command)
-        self.x += self.velocityx
-        self.y += self.velocityy
+    def update(self, direction):
+        if direction == "up":
+            self.clip(self.backWalk)
+        if direction == "down":
+            self.clip(self.walk)
+        if direction == "right":
+            self.clip(self.rightWalk)
 
-    def draw(self, screen=pygame.display.set_mode((1280, 720))):
-        screen.blit(self.actualFrame, (screen.get_window_size()[0] / 2))
-
-    def rezise(self, screen):  # screen = (width, height)
-        self.realX = (screen[0] - self.actualFrame.get_width) / 2
-        self.realY = (screen[1] - self.actualFrame.get_height) / 2
-
-    def states(self, command):
-        self.commands.get(command)
-
-    def up(self):
-        self.side = 0
-        self.walk()
-
-    def DOWN(self):
-        self.side = 1
-        self.walk()
-
-    def RIGHT(self):
-        self.side = 2
-        self.walk()
-
-    def LEFT(self):
-        self.side = 3
-        self.walk()
-
-    def wait(self):
-        self.velocityx = 0
-        self.velocityy = 0
-        self.actualAnimation = self.animations[self.side]
-
-    def walk(self):
-        m = self.switcher.get(self.side)
-        self.velocityx = m[0]
-        self.velocityy = m[1]
-
-    def createAnimations(self):
-        pass
