e926d72d6582882a8f37a281b0c99aad674cc29c
R2d2cruz <51866042+R2d2cruz@users.noreply.github.com>
2020-03-21 12:17:26 -0500

    create background

---
+++ Background.py
@@ -0,0 +1,45 @@
+import pygame
+
+
+class BackGroundMap:
+    map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    rect = 32
+    frames = {
+
+    }
+    #
+    # 0 = grass
+    # 1 = bricks
+    # 20x9
+    #
+    #
+
+    xinit = 0
+    yinit = 0
+
+    def __init__(self):
+        image = pygame.image.load("RPG Nature Tileset.png")
+        self.frames = {
+            0: pygame.transform.chop(image, (0, 64, 32, 32)),
+            1: pygame.transform.chop(image, (64, 64, 32, 32))
+        }
+
+    def changeCoor(self, x, y):
+        self.xinit -= x
+        self.yinit -= y
+
+    def blit(self, screen):
+        for i in range(len(self.map)):
+            for j in range(len(self.map[0])):
+                screen.blit(self.frames.get(self.map[i][j]), (self.xinit + (32 * j), self.yinit + (32 * i)))
+
