3ba9af748d129693f124702f756543784ffb5160
R2d2cruz <51866042+R2d2cruz@users.noreply.github.com>
2020-03-26 08:38:43 -0500

    Create Objects.py

---
+++ Objects.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+class Objects:
+
+    rect = pygame.Rect((0, 0, 32, 32))
+    frames = [
+
+    ]
+    i = 0
+
+    def __init__(self, x, y, *groups):
+        super().__init__(*groups)
+        self.rect.topleft = [x, y]
+        self.x = x
+        self.y = y
+
+    def blit(self, screen):
+        screen.blit(self.frames[self.i], self.rect)
+
+    def get_rect(self):
+        return self.rect
+
+    def change_reference_point(self, position):
+        self.rect.topleft = [self.x + position[0], self.y + position[1]]
+
+
+class Rock(Objects):
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        image = pygame.image.load("RPG Nature Tileset.png")
+        self.frames = {
+            0: image.subsurface((64, 32, 32, 32))
+        }
+
